14bd8d18902750b014d33101ee8e5e6467bd3857
mementis <menasivak@gmail.com>
2021-01-31 20:34:05 +0100

    menu & carousel

---
+++ pticici.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    console.log(" ok");
+    $('#carouselslide').carousel({
+        
+        interval: 5000
+      })
+
+    // jQuery methods go here...
+  
+  });
