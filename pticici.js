d1cb5c1ef441fae1371bdd9b96759e6003b32701
mementis <menasivak@gmail.com>
2021-02-11 00:25:04 +0100

    improved thumbnail carousel, chevrons and all

--- pticici.js
+++ pticici.js
@@ -8,11 +8,26 @@ $(document).ready(function(){
 
     // jQuery methods go here...
 
-    console.log(" ok");
-    $('#myCarousel').carousel({
-        
+    
+    $('#mySlideshow').carousel({
+        pause:true,
+        interval: 15000
         
       })
+      $(".item1").click(function(){
+        $("#mySlideshow").carousel(0);
+      });
+      $(".item2").click(function(){
+        $("#mySlideshow").carousel(1);
+      });
+      
+
+      $(".carousel-control-prev").click(function(){
+        $("#mySlideshow").carousel("prev");
+      });
+      $(".carousel-control-next").click(function(){
+        $("#mySlideshow").carousel("next");
+      });
     /*function myFunction() {
       document.getElementById("carouselslide").style.minHeight = "200px";
     }*/
